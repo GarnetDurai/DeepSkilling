d827a8a5348043e424ff61cf790f2b48b4e22da7
garnetdurai <garnetdurai060805@gmail.com>
2026-06-10 18:04:30 +0530

    ECommerce

--- week 1/EcommercePlatformSearchFunction/src/client.java
+++ week 1/EcommercePlatformSearchFunction/src/client.java
@@ -1,16 +1,7 @@
 import java.util.Arrays;
 
 public class client {
-    public static void main(String[]args){
-        Product products[] = {
-                new Product(101, "IronMan", "Toy"),
-                new Product(203, "Lipstick", "MakeUp"),
-                new Product(102, "Apple", "fruit"),
-                new Product(473, "Coke", "SoftDrink"),
-                new Product(103, "pencil", "Stationary"),
-                new Product(111, "Shampoo", "haircare")
-        };
-        int target = 473;
+    public static void linearSearch(Product products[], int target){
         System.out.println("Linear Search");
         System.out.println();
         for(int i=0; i<products.length; i++){
@@ -18,10 +9,12 @@ public class client {
                 System.out.println("Found product with Id : " + target);
                 System.out.println("Product name : " + products[i].productName);
                 System.out.println("Product category : " + products[i].category);
-                break;
+                return;
             }
         }
-        System.out.println("------------------------------------------");
+        System.out.println("Product with id : " + target + " not found");
+    }
+    public static void binarySearch(Product products[], int target){
         System.out.println("Binary Search");
         System.out.println();
         Arrays.sort(products, (a,b) -> a.productId - b.productId);
@@ -35,7 +28,7 @@ public class client {
                 System.out.println("Found product with Id : " + target);
                 System.out.println("Product name : " + products[mid].productName);
                 System.out.println("Product category : " + products[mid].category);
-                break;
+                return;
             }
             else if(target < products[mid].productId){
                 j = mid - 1;
@@ -43,6 +36,21 @@ public class client {
                 i = mid + 1;
             }
         }
+        System.out.println("Product with id : " + target + " not found");
+    }
+    public static void main(String[]args){
+        Product products[] = {
+                new Product(101, "IronMan", "Toy"),
+                new Product(203, "Lipstick", "MakeUp"),
+                new Product(102, "Apple", "fruit"),
+                new Product(473, "Coke", "SoftDrink"),
+                new Product(103, "pencil", "Stationary"),
+                new Product(111, "Shampoo", "haircare")
+        };
+        int target = 473;
+        linearSearch(products, target);
+        System.out.println("------------------------------------------");
+        binarySearch(products, target);
 
 
     }
