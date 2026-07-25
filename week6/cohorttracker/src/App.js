7edcfe07ad3036edca3cb1554f94e6755526e864
garnetdurai <garnetdurai060805@gmail.com>
2026-07-25 14:27:32 +0530

    Done Cohort

---
+++ week6/cohorttracker/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import CohortDetails from "./CohortDetails"
+function App() {
+  return (
+    <div className="App">
+      <CohortDetails />
+    </div>
+  );
+}
+
+export default App;
