75a1c4f12e955808b8f8b2cdbadc52b2683decf0
garnetdurai <garnetdurai060805@gmail.com>
2026-07-27 18:41:01 +0530

    Rental App

---
+++ week6/officespacerentalapp/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Office from "./Office";
+
+function App() {
+  return (
+   <div>
+     <h1>Office Space Rental App</h1>
+   <Office/>
+   </div>
+  );
+}
+
+export default App;
